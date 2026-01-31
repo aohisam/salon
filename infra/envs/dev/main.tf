2100eec0948cd6479573b81409dec7ec48ca3b6a
aohisam <second.2053.tjpamw@outlook.jp>
2026-01-31 11:19:23 +0900

    fix envs-dev-main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.6.0"
-  required_providers {
-    terraform = {
-      source  = "hashicorp/terraform"
-      version = "~> 1.0"
-    }
-  }
 }
 
 resource "terraform_data" "marker" {
