f9eca97f65251dbf713896efd388637d949e326a
aohisam <second.2053.tjpamw@outlook.jp>
2026-01-31 12:44:28 +0900

    fix envs/*/backend.tf destroy dynamoDB

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "tfstate-dev-my-saas-894923172428"
     key            = "envs/dev/terraform.tfstate"
     region         = "ap-northeast-1"
-    dynamodb_table = "tf-lock-dev-my-saas"
+    use_lockfile   = true
     encrypt        = true
     kms_key_id     = "arn:aws:kms:ap-northeast-1:894923172428:key/1000dd13-1cfc-43a6-ace1-cb9b0aad4058"
   }
--- infra/envs/stg/backend.tf
+++ infra/envs/stg/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "tfstate-stg-my-saas-894923172428"
     key            = "envs/stg/terraform.tfstate"
     region         = "ap-northeast-1"
-    dynamodb_table = "tf-lock-stg-my-saas"
+    use_lockfile   = true
     encrypt        = true
     kms_key_id     = "arn:aws:kms:ap-northeast-1:894923172428:key/4e969044-269b-40fc-aa89-2d7360e08eae"
   }
