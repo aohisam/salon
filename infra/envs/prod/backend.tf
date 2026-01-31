d10b3c9f39a148dd2bc151f4ef99e9cd321da30f
aohisam <second.2053.tjpamw@outlook.jp>
2026-01-31 12:48:27 +0900

    fix envs/*/main.tf recursive

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstate-prod-my-saas-581059493336"
-    key            = "envs/prod/terraform.tfstate"
-    region         = "ap-northeast-1"
-    use_lockfile   = true
-    encrypt        = true
-    kms_key_id     = "arn:aws:kms:ap-northeast-1:581059493336:key/a4bebf8a-61ad-42e5-8c81-5620fdc25b22"
+    bucket       = "tfstate-prod-my-saas-581059493336"
+    key          = "envs/prod/terraform.tfstate"
+    region       = "ap-northeast-1"
+    use_lockfile = true
+    encrypt      = true
+    kms_key_id   = "arn:aws:kms:ap-northeast-1:581059493336:key/a4bebf8a-61ad-42e5-8c81-5620fdc25b22"
   }
 }
